03ac0648100b3ec0b4b7600a67f62bc6633db15c
BonzoFestoon <gabrielsmith1973+github@gmail.com>
2017-07-07 16:18:23 -0400

    New project to display images and location on map with GPS information stored in the metadata

--- ShowPhotoLocation/Form1.Designer.cs
+++ ShowPhotoLocation/Form1.Designer.cs
@@ -28,12 +28,160 @@
       /// </summary>
       private void InitializeComponent()
       {
-         this.components = new System.ComponentModel.Container();
+         this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+         this.treeView1 = new System.Windows.Forms.TreeView();
+         this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+         this.rasterPictureBox1 = new Leadtools.Controls.RasterPictureBox();
+         this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+         this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+         this.labelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+         this.labelStatusMapLink = new System.Windows.Forms.ToolStripStatusLabel();
+         this.labelStatusStreeViewLink = new System.Windows.Forms.ToolStripStatusLabel();
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+         this.splitContainer1.Panel1.SuspendLayout();
+         this.splitContainer1.Panel2.SuspendLayout();
+         this.splitContainer1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+         this.splitContainer2.Panel1.SuspendLayout();
+         this.splitContainer2.Panel2.SuspendLayout();
+         this.splitContainer2.SuspendLayout();
+         this.statusStrip1.SuspendLayout();
+         this.SuspendLayout();
+         // 
+         // splitContainer1
+         // 
+         this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+         this.splitContainer1.Name = "splitContainer1";
+         // 
+         // splitContainer1.Panel1
+         // 
+         this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+         // 
+         // splitContainer1.Panel2
+         // 
+         this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+         this.splitContainer1.Size = new System.Drawing.Size(793, 709);
+         this.splitContainer1.SplitterDistance = 288;
+         this.splitContainer1.TabIndex = 0;
+         // 
+         // treeView1
+         // 
+         this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.treeView1.Location = new System.Drawing.Point(0, 0);
+         this.treeView1.Name = "treeView1";
+         this.treeView1.Size = new System.Drawing.Size(288, 709);
+         this.treeView1.TabIndex = 0;
+         this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+         // 
+         // splitContainer2
+         // 
+         this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+         this.splitContainer2.Name = "splitContainer2";
+         this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+         // 
+         // splitContainer2.Panel1
+         // 
+         this.splitContainer2.Panel1.Controls.Add(this.rasterPictureBox1);
+         // 
+         // splitContainer2.Panel2
+         // 
+         this.splitContainer2.Panel2.Controls.Add(this.webBrowser1);
+         this.splitContainer2.Size = new System.Drawing.Size(501, 709);
+         this.splitContainer2.SplitterDistance = 229;
+         this.splitContainer2.TabIndex = 0;
+         // 
+         // rasterPictureBox1
+         // 
+         this.rasterPictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.rasterPictureBox1.Location = new System.Drawing.Point(0, 0);
+         this.rasterPictureBox1.Name = "rasterPictureBox1";
+         this.rasterPictureBox1.Size = new System.Drawing.Size(501, 229);
+         this.rasterPictureBox1.TabIndex = 1;
+         this.rasterPictureBox1.TabStop = false;
+         // 
+         // webBrowser1
+         // 
+         this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+         this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+         this.webBrowser1.Name = "webBrowser1";
+         this.webBrowser1.Size = new System.Drawing.Size(501, 476);
+         this.webBrowser1.TabIndex = 0;
+         // 
+         // statusStrip1
+         // 
+         this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labelStatus,
+            this.labelStatusMapLink,
+            this.labelStatusStreeViewLink});
+         this.statusStrip1.Location = new System.Drawing.Point(0, 687);
+         this.statusStrip1.Name = "statusStrip1";
+         this.statusStrip1.Size = new System.Drawing.Size(793, 22);
+         this.statusStrip1.TabIndex = 1;
+         this.statusStrip1.Text = "statusStrip1";
+         // 
+         // labelStatus
+         // 
+         this.labelStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.labelStatus.Name = "labelStatus";
+         this.labelStatus.Size = new System.Drawing.Size(39, 17);
+         this.labelStatus.Text = "Status";
+         // 
+         // labelStatusMapLink
+         // 
+         this.labelStatusMapLink.IsLink = true;
+         this.labelStatusMapLink.Name = "labelStatusMapLink";
+         this.labelStatusMapLink.Size = new System.Drawing.Size(109, 17);
+         this.labelStatusMapLink.Text = "Open Google Maps";
+         this.labelStatusMapLink.Visible = false;
+         this.labelStatusMapLink.Click += new System.EventHandler(this.LabelStatusMapLink_Click);
+         // 
+         // labelStatusStreeViewLink
+         // 
+         this.labelStatusStreeViewLink.IsLink = true;
+         this.labelStatusStreeViewLink.Name = "labelStatusStreeViewLink";
+         this.labelStatusStreeViewLink.Size = new System.Drawing.Size(138, 17);
+         this.labelStatusStreeViewLink.Text = "Open Google Street View";
+         this.labelStatusStreeViewLink.Click += new System.EventHandler(this.LabelStatusStreeViewLink_Click);
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(793, 709);
+         this.Controls.Add(this.statusStrip1);
+         this.Controls.Add(this.splitContainer1);
+         this.Name = "Form1";
          this.Text = "Form1";
+         this.Load += new System.EventHandler(this.Form1_Load);
+         this.splitContainer1.Panel1.ResumeLayout(false);
+         this.splitContainer1.Panel2.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+         this.splitContainer1.ResumeLayout(false);
+         this.splitContainer2.Panel1.ResumeLayout(false);
+         this.splitContainer2.Panel2.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+         this.splitContainer2.ResumeLayout(false);
+         this.statusStrip1.ResumeLayout(false);
+         this.statusStrip1.PerformLayout();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
       }
 
       #endregion
+
+      private System.Windows.Forms.SplitContainer splitContainer1;
+      private System.Windows.Forms.TreeView treeView1;
+      private System.Windows.Forms.SplitContainer splitContainer2;
+      private System.Windows.Forms.WebBrowser webBrowser1;
+      private System.Windows.Forms.StatusStrip statusStrip1;
+      private System.Windows.Forms.ToolStripStatusLabel labelStatus;
+      private Leadtools.Controls.RasterPictureBox rasterPictureBox1;
+      private System.Windows.Forms.ToolStripStatusLabel labelStatusMapLink;
+      private System.Windows.Forms.ToolStripStatusLabel labelStatusStreeViewLink;
    }
 }
 
--- ShowPhotoLocation/Form1.cs
+++ ShowPhotoLocation/Form1.cs
@@ -1,20 +1,217 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using Leadtools.Codecs;
+using Leadtools.Controls;
+using Leadtools;
+using Leadtools.Drawing;
 
 namespace ShowPhotoLocation
 {
    public partial class Form1 : Form
    {
+      private readonly string _rootPath = Application.StartupPath;
+
       public Form1()
       {
+         RasterSupport.SetLicense(
+            File.ReadAllBytes(@"C:\LEADTOOLS 19\Common\License\leadtools.lic"),
+            File.ReadAllText(@"C:\LEADTOOLS 19\Common\License\leadtools.lic.key")
+            );
+
          InitializeComponent();
+
+         webBrowser1.ScriptErrorsSuppressed = true;
+         webBrowser1.ScrollBarsEnabled = true;
+        
+         RasterPaintProperties paintProperties = RasterPaintProperties.Default;
+         paintProperties.PaintDisplayMode = RasterPaintDisplayModeFlags.Bicubic;
+         paintProperties.PaintEngine = RasterPaintEngine.GdiPlus;
+         paintProperties.UsePaintPalette = true;
+         rasterPictureBox1.PaintProperties = paintProperties;
+         rasterPictureBox1.SizeMode = RasterPictureBoxSizeMode.Fit;
       }
+
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         ListDirectory(treeView1, Path.Combine(_rootPath, "Images"));
+      }
+
+      private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+      {
+         UpdateStatusMessage("");
+         if (e.Node == null)
+            return;
+
+         string fullPath = Path.Combine(_rootPath, e.Node.FullPath);
+         
+         if (!File.Exists(fullPath))
+            return;
+
+         if (rasterPictureBox1.Image != null && !rasterPictureBox1.Image.IsDisposed)
+            rasterPictureBox1.Image.Dispose();
+
+         try
+         {
+            using (RasterCodecs codecs = new RasterCodecs())
+               rasterPictureBox1.Image = codecs.Load(fullPath);
+         }
+         catch (Exception exception)
+         {
+            ClearMap(e.Node.Text);
+            UpdateStatusMessage(exception.Message);
+            return;
+         }
+         
+         double? longitude = GetLongitude(fullPath);
+         double? latitude = GetLatitude(fullPath);
+
+         if (!longitude.HasValue || !latitude.HasValue)
+         {
+            ClearMap(e.Node.Text);
+            return;
+         }
+         labelStatusMapLink.Visible = true;
+
+         UpdateStatusMessage($@"Coordinates for {e.Node.Text}: {latitude:F5}, {longitude:F5}");
+         UpdateStatusMapLink(latitude.Value, longitude.Value);
+
+         string targetHtmlFile = Path.Combine(Application.StartupPath, "map.html");
+         CreateHtmlFile("MapTemplate.html", targetHtmlFile, latitude.ToString(), longitude.ToString(), e.Node.Name, "", "", fullPath);
+
+         webBrowser1.Url = new Uri(targetHtmlFile);
+      }
+
+      private static void ListDirectory(TreeView treeView, string path)
+      {
+         treeView.Nodes.Clear();
+         var rootDirectoryInfo = new DirectoryInfo(path);
+         treeView.Nodes.Add(CreateDirectoryNode(rootDirectoryInfo));
+      }
+
+      private static TreeNode CreateDirectoryNode(DirectoryInfo directoryInfo)
+      {
+         var directoryNode = new TreeNode(directoryInfo.Name);
+         foreach (var directory in directoryInfo.GetDirectories())
+            directoryNode.Nodes.Add(CreateDirectoryNode(directory));
+         foreach (var file in directoryInfo.GetFiles())
+            directoryNode.Nodes.Add(new TreeNode(file.Name));
+         return directoryNode;
+      }
+
+      private void ClearMap(string fileName)
+      {
+         labelStatusMapLink.Visible = false;
+         webBrowser1.Url = new Uri("about:blank");
+         UpdateStatusMessage($@"No GPS information found in {fileName}");
+      }
+
+      private void UpdateStatusMessage(string message)
+      {
+         labelStatus.Text = message;
+      }
+
+      private void UpdateStatusMapLink(double latitude, double longitude)
+      {
+         
+         labelStatusMapLink.Tag = $@"http://maps.google.com/?q={latitude},{longitude}";
+         labelStatusStreeViewLink.Tag =
+            $@"http://maps.google.com/?q=&layer=c&cbll={latitude},{longitude}&cbp=11,0,0,0,0";
+      }
+
+      private static double? GetLongitude(string imagePath)
+      {
+         try
+         {
+            using (var codecs = new RasterCodecs())
+            {
+               RasterMetadataURational[] data = 
+                  codecs.ReadComment(imagePath, 1, RasterCommentMetadataType.GpsLongitude)?.ToURational();
+               string refData = 
+                  codecs.ReadComment(imagePath, 1, RasterCommentMetadataType.GpsLongitudeRef)?.ToAscii();
+               return ExifGpsToDouble(refData, data);
+            }
+         }
+         catch (ArgumentException)
+         {
+            return null;
+         }
+      }
+
+      private static double? GetLatitude(string imagePath)
+      {
+         try
+         {
+            using (var codecs = new RasterCodecs())
+            {
+               RasterMetadataURational[] data =
+                  codecs.ReadComment(imagePath, 1, RasterCommentMetadataType.GpsLatitude)?.ToURational();
+               string refData =
+                  codecs.ReadComment(imagePath, 1, RasterCommentMetadataType.GpsLatitudeRef)?.ToAscii();
+               return ExifGpsToDouble(refData, data);
+            }
+         }
+         catch (ArgumentException)
+         {
+            return null;
+         }
+      }
+
+      private static double? ExifGpsToDouble(string itemRef, RasterMetadataURational[] items)
+      {
+         if (string.IsNullOrWhiteSpace(itemRef) || items == null)
+            throw new ArgumentException();
+
+         double degrees = (double) items[0].Numerator / items[0].Denominator;
+         if (degrees > 360) return null;
+         double minutes = (double) items[1].Numerator / items[1].Denominator;
+         double seconds = (double) items[2].Numerator / items[2].Denominator;
+
+         double coordinate = degrees + (minutes / 60d) + (seconds / 3600d);
+         
+         if (itemRef == "S" || itemRef == "W")
+            coordinate = coordinate * -1;
+
+         return coordinate;
+      }
+
+      private static void CreateHtmlFile(string templateFile, string targetFile, string la, string lo,
+                                        string title, string city, string marker, string imagePath)
+      {
+         if (!File.Exists(templateFile)) return;
+         using (StreamReader reader = new StreamReader(templateFile))
+         {
+            string htmlString = reader.ReadToEnd()
+                                      .Replace("[la]", la)
+                                      .Replace("[lo]", lo)
+                                      .Replace("[title]", title)
+                                      .Replace("[city]", city)
+                                      .Replace("[image]", imagePath)
+                                      .Replace("[marker]", marker);
+            using (var writer = new StreamWriter(targetFile))
+               writer.Write(htmlString);
+         }
+      }
+
+      private void LabelStatusMapLink_Click(object sender, EventArgs e)
+      {
+         // if google maps gives you grief about the webbrowser control being too old,
+         // read this and make it ie11: 
+         // https://weblog.west-wind.com/posts/2011/May/21/Web-Browser-Control-Specifying-the-IE-Version
+         string url = labelStatusMapLink.Tag as string;
+         if (url == null) return;
+         webBrowser1.Url = new Uri(url);
+      }
+
+      private void LabelStatusStreeViewLink_Click(object sender, EventArgs e)
+      {
+         string url = labelStatusStreeViewLink.Tag as string;
+         if (url == null) return;
+         //webBrowser1.Url = new Uri(url);
+         System.Diagnostics.Process.Start(url);
+      }
+
+
    }
 }
